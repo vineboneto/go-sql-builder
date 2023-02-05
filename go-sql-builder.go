eddb0d29b774e41746227dc66756b14705086b48
vineboneto <vineboneto@gmail.com>
2023-02-05 10:53:09 -0300

    fix: check params has value correct form

--- go-sql-builder.go
+++ go-sql-builder.go
@@ -32,7 +32,7 @@ func BuildPG() *Query {
 }
 
 func (q *Query) InsertOnlyValue(field string, v any) *Query {
-	if v != "" && v != 0 && v != nil {
+	if q.checkHashValue(v) {
 		q.insertFieldSql = append(q.insertFieldSql, field)
 		q.args = append(q.args, v)
 	}
@@ -48,7 +48,7 @@ func (q *Query) Insert(field string, v any) *Query {
 }
 
 func (q *Query) UpdateOnlyValue(field string, v any) *Query {
-	if v != "" && v != 0 && v != nil {
+	if q.checkHashValue(v) {
 		q.updateFieldsSql = append(q.updateFieldsSql, field)
 		q.args = append(q.args, v)
 	}
@@ -90,7 +90,7 @@ func (q *Query) AndRaw(raw string) *Query {
 
 func (q *Query) And(s string, v any) *Query {
 
-	if v != "" && v != 0 && v != nil && v != false {
+	if q.checkHashValue(v) {
 		q.whereFieldsSql = append(q.whereFieldsSql, s)
 		q.args = append(q.args, v)
 	}
@@ -116,7 +116,7 @@ func (q *Query) AndLike(s string, v string) *Query {
 
 func (q *Query) AndBetween(s string, v1 any, v2 any) *Query {
 
-	if v1 != "" && v1 != 0 && v1 != nil && v2 != "" && v2 != 0 && v2 != nil {
+	if q.checkHashValue(v1) && q.checkHashValue(v2) {
 		q.whereFieldsSql = append(q.whereFieldsSql, s)
 		q.args = append(q.args, v1, v2)
 	}
@@ -214,4 +214,28 @@ func (q *Query) String() (string, []any) {
 	trimJoin := strings.TrimSpace(strings.Join(str, " "))
 
 	return pattern.ReplaceAllString(trimJoin, " "), q.args
+}
+
+func (q *Query) checkHashValue(v any) bool {
+	switch v := v.(type) {
+	case int:
+		if v != 0 {
+			return true
+		}
+	case string:
+		if v != "" {
+			return true
+		}
+	case float64:
+		if v != float64(0) {
+			return true
+		}
+	case bool:
+		return v
+	case nil:
+		return false
+	default:
+		return false
+	}
+	return false
 }
